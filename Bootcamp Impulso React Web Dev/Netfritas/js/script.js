2aff98a8dba5da9224559a20c7c8f32e6d63cc99
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2021-09-29 15:43:26 -0300

    Final touches

--- Bootcamp Impulso React Web Dev/Netfritas/js/script.js
+++ Bootcamp Impulso React Web Dev/Netfritas/js/script.js
@@ -4,3 +4,20 @@ $(function() {
      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
    });
  });
+
+//  Scrolling fade
+
+function scrollFade() {
+   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+   var scrolled = (height / winScroll) - 1.75;
+   document.getElementById("scroll").style.opacity = scrolled;
+}
+
+document.addEventListener("scroll", scrollFade)
+
+// Show temporary page on click
+
+function popUp() {
+   
+}
