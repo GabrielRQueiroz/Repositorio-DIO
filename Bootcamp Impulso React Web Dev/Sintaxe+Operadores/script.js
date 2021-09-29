56a5eacf0e82466718eb061e0e1146b184798a45
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2021-09-29 16:30:58 -0300

    New activity from the bootcamp

---
+++ Bootcamp Impulso React Web Dev/Sintaxe+Operadores/script.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')();
+ 
+let x = prompt("Insert X's value as an integer:");
+let y = prompt("Now, insert Y's value as an integer:");
+
+console.log(compareNumbers(x, y))
+
+function compareNumbers(x, y) {
+   x = parseInt(x);
+   y = parseInt(y);
+
+   let equals = x==y;
+   let sum = x+y;
+
+   let compareTo10 = "";
+   let compareTo20 = "";
+
+   let case1 = ""
+   let case2 = `Their sum equals ${sum}. `; 
+   // It means it is ${compareTo10} than 10 and ${compareTo20} than 20.
+
+   equals === true ? case1 = `The numbers ${x} and ${y} are the same. ` : case1 = `The numbers ${x} and ${y} are NOT the same. `;
+   
+   sum < 10 ? case2 += "It means it is less than 10 " : case2 += "It means it is greater or equal to 10 ";
+   
+   sum < 20 ? case2 += "and is less than 20." : case2 += "and is greater or equal to 20.";
+
+   return case1 + case2
+}
