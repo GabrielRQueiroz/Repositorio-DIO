4de93b0541f83338256d39bd56517c6a4e8bb175
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2021-10-04 15:20:28 -0300

    Dark mode :O

---
+++ Bootcamp Impulso React Web Dev/Basecamp-JS/Modules/js/functions.mjs
@@ -0,0 +1,25 @@
+function displayAge(name, age) {
+
+   return `${name} is ${age} yeas old.`;
+
+}
+
+function displayCity(name, city) {
+
+   return `${name} lives in ${city}.`;
+
+}
+
+function displayHobby(name, hobby) {
+
+   return `${name} has ${hobby} as their hobby.`;
+
+}
+
+export {
+
+   displayAge,
+   displayCity,
+   displayHobby
+
+}
---
+++ Bootcamp Impulso React Web Dev/Basecamp-JS/Modules/js/main.js
@@ -0,0 +1,5 @@
+import { displayAge, displayCity, displayHobby } from './functions.mjs';
+
+console.log(displayAge("Gabriel", 18));
+console.log(displayCity("Joelma", "Brasília"));
+console.log(displayHobby("Alan", "Soccer"));
