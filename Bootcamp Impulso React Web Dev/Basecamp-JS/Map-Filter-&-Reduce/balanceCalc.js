737696d3432617969e285551aafd7892d01526a7
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2021-10-05 19:35:53 -0300

    Added balance calculator

---
+++ Bootcamp Impulso React Web Dev/Basecamp-JS/Map-Filter-&-Reduce/balanceCalc.js
@@ -0,0 +1,28 @@
+const list = [
+   {
+      name: 'Street Fighter III',
+      price: 15,
+   },
+   {
+      name: 'Streets of Rage',
+      price: 20,
+   },
+   {
+      name: 'Golden Axe',
+      price: 18,
+   },
+   {
+      name: 'Altered Beast',
+      price: 20,
+   },
+
+];
+
+const availableBalance = 80;
+
+function balanceCalc(availableBalance, list) {
+   return list.reduce(function (prev, current, index) {
+      return prev-current.price;
+   }, availableBalance);
+
+console.log(`${balanceCalc(availableBalance, list)}$ remaining`);
