5b22fc3b98ba4555b83a5a2ec1fc1484c6695fc6
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2021-12-12 18:40:59 -0300

    Finished training

--- Bootcamp Inter/node-user-auth/src/index.ts
+++ Bootcamp Inter/node-user-auth/src/index.ts
@@ -1,11 +1,22 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// rotas
+app.use(usersRoute);
+app.use(statusRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
 	res.status(200).send({ foo: 'lesgou' });
 });
 
+// inicialização
 app.listen(3000, () => {
 	console.log('Aplicação executando na porta 3000...');
 });
